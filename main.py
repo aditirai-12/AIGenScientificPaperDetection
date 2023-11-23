5d8ebf749bc30f0eb6d82ef6ae3138b330f06cc8
Aditi  Rai <aditirai@MacBook-Pro.local>
2023-11-22 19:57:50 -0700

    tokenized and encoded text in training, validation, and test datasets; converted tokenized list to tensors for training, validation, and test; dataloaders for training and validation; created BERTArchitecture(nn.Module) class

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import transformers
-from transformers import AutoModel, BertTokenizerFast
+from transformers import AutoModel, BertTokenizerFast, AdamW
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
-# specify GPU
+#specify GPU or CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #load datasets and create dataframe
@@ -22,8 +23,8 @@ train_df['text'] = 'Title: ' + train_df['title'] + ' Abstract: ' + train_df['abs
 test_df['text'] = 'Title: ' + test_df['title'] + ' Abstract: ' + test_df['abstract'] + ' Introduction: ' + test_df['introduction']
 
 #split training data into train and validation sets
-train_x, val_x, train_y, val_y = train_test_split(train_df['text'], train_df['label'], test_size=0.2, random_state=42)
-test_x = test_df['text']
+train_text, val_text, train_label, val_label = train_test_split(train_df['text'], train_df['label'], test_size=0.2, random_state=42)
+test_text = test_df['text']
 
 #import BERT-base pretrained model
 bert = AutoModel.from_pretrained('bert-base-uncased')
@@ -31,14 +32,68 @@ bert = AutoModel.from_pretrained('bert-base-uncased')
 #load BERT tokenizer
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-#data prep
+#tokenize and enocde text in training set
+tokens_train = tokenizer.batch_encode_plus(train_text.tolist(), max_length=512, padding=True, truncation=False)
 
-#create datasets and dataloaders
+#tokenize and encode text in validation set
+tokens_val = tokenizer.batch_encode_plus(val_text.tolist(), max_length=512, padding=True, truncation=False)
 
-#model architecture
+#tokenize and encode text in test set
+tokens_test = tokenizer.batch_encode_plus(test_text.tolist(), max_length=512, padding=True, truncation=False)
 
-#training
+#convert lists to tensors
+train_seq = torch.tensor(tokens_train['input_ids'])
+train_mask = torch.tensor(tokens_train['attention_mask'])
+train_y = torch.tensor(train_label.tolist())
 
-#evaluation
+val_seq = torch.tensor(tokens_val['input_ids'])
+val_mask = torch.tensor(tokens_val['attention_mask'])
+val_y = torch.tensor(val_label.tolist())
 
+test_seq = torch.tensor(tokens_test['input_ids'])
+test_mask = torch.tensor(tokens_test['attention_mask'])
+
+#wrap training tensors
+train_data = TensorDataset(train_seq, train_mask, train_y)
+
+#sample data during training
+train_sample = RandomSampler(train_data)
+
+#dataloader for training dataset
+train_dataloader = DataLoader(train_data, sampler=train_sample, batch_size=16)
+
+#wrap validation tensors
+val_data = TensorDataset(val_seq, val_mask, val_y)
+
+#sample validation data
+val_sample = RandomSampler(val_data)
+
+#dataloader for validation dataset
+val_dataloader = DataLoader(val_data, sampler=val_sample, batch_size=16)
+
+#set params of BERT model to not require gradients so only params of added layers trained
+for param in bert.parameters():
+    param.requires_grad = False
+
+class BERTArchitecture(nn.Module):
+
+    def __init__(self, bert):
+        super(BERTArchitecture, self).__init__()
+        self.bert = bert
+        self.dropout = nn.Dropout(0.1)
+        self.fc = nn.Linear(768, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, sent_id, mask):
+        _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
+        x = self.dropout(cls_hs)
+        x = self.fc(x)
+        x = self.sigmoid(x)
+        return x 
+
+#pass the pre-trained BERT to BERTArchitecture()
+model = BERTArchitecture(bert)
+
+#model to device indicated in beginning of code
+model = model.to(device)
 
